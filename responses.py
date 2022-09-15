9bf688f6b26fa6161d4df345f130e1f467d5ae65
seabuck211 <113558329+seabuck211@users.noreply.github.com>
2022-09-15 05:57:19 +0000

    Initial commit

---
+++ responses.py
@@ -0,0 +1,15 @@
+import random
+
+
+def handle_response(message) -> str:
+    p_message = message.lower()
+    if p_message == 'hello':
+        return 'Hey there!'
+
+    if p_message == 'roll':
+        return str(random.randint(1, 6))
+
+    if p_message == '!help':
+        return "`Dongers.`"
+
+    #  return 'Yeah, I don\'t know. Try typing "!help".'
